0b09cb1f044c884f70bc130496ce3d0b44954b5c
Dragos AIOANEI <daioanei@ias.bitdefender.biz>
2017-04-02 18:25:11 +0300

    Stage 1

---
+++ site-root/.ctrl/index.py
@@ -0,0 +1,9 @@
+import time
+import random
+
+class IndexController(object):
+    def __init__(self):
+        super(IndexController, self).__init__
+
+    def get_view(self):
+        return ".view/index.html"
