c63e0196c5702a352e9c9c047f1d37c102a989b5
ted_996 <ebudaca@ias.bitdefender.biz>
2017-04-05 20:35:01 +0300

    Basic console controller

---
+++ site-root/.py/db_access/db_ops.py
@@ -0,0 +1,5 @@
+import cx_Oracle
+
+def get_connection():
+	ip = "127.0.0.1"
+	raise NotImplementedError()
--- site-root/index.py
+++ site-root/index.py
@@ -1,3 +1,3 @@
-import from ctrl import index
+from ctrl import index
 mvc.set_init_ctrl(index.IndexController())
 
