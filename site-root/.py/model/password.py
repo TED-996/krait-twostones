592c98ab031793e91b17246a894daca47af6cd34
TED-996 <budaca.eduard@gmail.com>
2017-04-11 06:10:06 +0000

    Fixed error in password.py

--- site-root/.py/model/password.py
+++ site-root/.py/model/password.py
@@ -4,7 +4,7 @@ import base64
 
 def to_salted_hash(password, salt=None):
 	if salt is None:
-		salt = os.urandom(16).encode(hex)
+		salt = os.urandom(16).encode("hex")
 	
 	return salt + '-' + hashlib.sha256(salt + password).hexdigest()
 
@@ -13,3 +13,4 @@ def check_password(password, salted_hash):
 	salt_idx = salted_hash.index('-')
 	salt = salted_hash[:salt_idx]
 	return to_salted_hash(password, salt) == salted_hash
+
