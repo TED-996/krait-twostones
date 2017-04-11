7e5f4cb269596b9e0985ae72458efde4a73f2a85
mihaivasilache <mihai.vasilache12@yahoo.com>
2017-04-11 15:02:06 +0300

    Tweeked create and populate tables

--- createSqlDb/populateUser.sql
+++ createSqlDb/populateUser.sql
@@ -5,16 +5,20 @@ declare
 v_contor INTEGER := 0;
 v_player_id number;
 v_username player.playername%type;
-v_password player.password%type;
+v_password player.password%type := 'a';
+v_loadoutId loadout.id%type := NULL;
+v_inMatch PLAYER.INMATCH%type := 0;
+v_mmr PLAYER.MMR%type := 1000;
+v_level PLAYER.PLAYERLEVEL%type := 1;
 begin
   DBMS_OUTPUT.PUT_LINE('preinserts');
 	for v_contor in 1..10000 loop
-    v_player_id := playerIdSeq.nextVal;
+    v_player_id := v_contor;
     select playername into v_username
       from temp
       where id = v_player_id;
-    v_password := dbms_random.string('u',12);
-    user_ops.addPlayer(v_username,v_password);
+      v_password := dbms_random.string('u',12);
+      user_ops.addPlayer(v_username,v_password);
     --DBMS_OUTPUT.PUT_LINE('inserted one');
 	end loop;
   dbms_output.put_line('done!');
