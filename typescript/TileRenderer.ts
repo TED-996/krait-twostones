05f391ba64c1d74b0a2e9ad8bd051b4846570c95
ted_996 <ebudaca@ias.bitdefender.biz>
2017-04-25 23:44:20 +0300

    Work on renderer

---
+++ typescript/TileRenderer.ts
@@ -0,0 +1,138 @@
+/// <reference path="node_modules/@types/phaser/phaser.d.ts" />
+
+interface TileSource {
+    getTiles() : Tile[];
+}
+
+interface SortedTileSource {
+    getSortedTiles() : Tile[];
+}
+
+
+class ImmutableTileSource implements TileSource, SortedTileSource {
+    tiles : Tile[];
+
+    constructor(tiles: Tile[]) {
+        this.tiles = TileSourceUtils.sort(tiles);
+    }
+
+    getTiles(): Tile[] {
+        return this.tiles;
+    }
+
+    getSortedTiles(): Tile[] {
+        return this.tiles;
+    }
+
+}
+
+class SortingTileSource implements SortedTileSource, TileSource {
+    source : TileSource;
+
+    constructor(source : TileSource){
+        this.source = source;
+    }
+
+    getTiles(): Tile[] {
+        return this.source.getTiles();
+    }
+
+    getSortedTiles(): Tile[] {
+        return TileSourceUtils.sort(this.source.getTiles());
+    }
+}
+
+class TileSourceUtils {
+    static sort(src: Tile[]) : Tile[]{
+        return src.sort(TileSourceUtils.tileComparator);
+    }
+
+    static concatSort(sources : TileSource[]) : Tile[]{
+        if (sources.length == 0){
+            return [];
+        }
+        if (sources.length == 1){
+            return TileSourceUtils.sort(sources[0].getTiles());
+        }
+        return TileSourceUtils.concatMerge(
+            sources.map(s => new ImmutableTileSource(s.getTiles()))); //ImmutableTileSource first sorts them
+    }
+
+    static concatMerge(sources : SortedTileSource[]) : Tile[] {
+        if (sources.length == 0){
+            return [];
+        }
+        if (sources.length == 1){
+            return sources[0].getSortedTiles();
+        }
+
+        let result = [];
+        let tileSources = sources.map(t => t.getSortedTiles()).filter(t => t.length != 0);
+        let source = tileSources[0];
+        let next = tileSources[0][0];
+        while(tileSources.length != 0){
+            source = tileSources[0];
+
+            let idx = 1;
+            while(idx < tileSources.length){
+                if (TileSourceUtils.tileComparator(tileSources[idx][0], next) == -1){
+                    next = tileSources[idx][0];
+                    source = tileSources[idx];
+                }
+            }
+
+            result.push(source.shift());
+            if (source.length == 0){
+                delete tileSources[tileSources.indexOf(source)];
+            }
+        }
+
+        return result;
+    }
+
+    static tileComparator(t1 : Tile, t2: Tile) : number{
+        if (t1.y != t2.y){
+            return t1.y > t2.y ? 1 : -1;
+        }
+        if (t1.x != t2.x){
+            return t1.x > t2.x ? 1 : -1;
+        }
+        if (t1.zIndex != t2.tileIndex){
+            return t1.zIndex > t2.zIndex ? 1 : -1;
+        }
+        if (t1.tileIndex != t2.tileIndex){
+            return t1.tileIndex > t2.tileIndex ? 1 : -1; //prevent any kind of flickering
+        }
+        return 0; //They are equal
+    }
+}
+
+
+class TileRenderer {
+    sources : SortedTileSource[];
+    group : Phaser.Group;
+    game : Phaser.Game;
+
+    constructor(immutableSources : TileSource[],
+                sortedSources : SortedTileSource[],
+                mutableSources : TileSource[],
+                game : Phaser.Game){
+        this.sources =
+            [<SortedTileSource>new ImmutableTileSource(TileSourceUtils.concatSort(immutableSources))]
+                .concat(sortedSources).concat(mutableSources.map(s => new SortingTileSource(s)));
+
+        this.group = game.add.group(null, "tiles", true, false);
+        this.init();
+    }
+
+    private init() : void {
+        this.update();
+    }
+
+    public update() : void{
+        this.group.removeAll();
+        for (let t  of TileSourceUtils.concatMerge(this.sources)){
+            this.group.add(new Phaser.Spri)
+        }
+    }
+}
