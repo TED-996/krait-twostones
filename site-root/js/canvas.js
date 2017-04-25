85ca7d1434297f114298076d689c4cae3a8aaff5
Maganu Mihai <Maganu Mihai>
2017-04-25 19:37:31 +0300

    dashboard upgrade

--- site-root/js/canvas.js
+++ site-root/js/canvas.js
@@ -24,3 +24,18 @@ function closeNav() {
     document.getElementById("loadoutMenu").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for( i = 0; i<acc.length; i++) {
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    }
+}
