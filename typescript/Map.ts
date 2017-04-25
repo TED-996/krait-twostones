64be2f769efad3ced8903dfc8074dbd77dc94d7e
mihaivasilache <mihai.vasilache12@yahoo.com>
2017-04-26 00:09:23 +0300

    Modified Map.tx

--- typescript/Map.ts
+++ typescript/Map.ts
@@ -45,12 +45,11 @@ class Map {
     constructor(map_url){
         let map_data = ajax_raw_sync(map_url);
         let map_json = JSON.parse(map_data);
-        let tempTileset = JSON.parse(map_json.tilesets);
-        this.tileset = new Tileset(tempTileset.image,tempTileset.firstgid,tempTileset.columns,tempTileset.spacing,tempTileset.tileheight,tempTileset.tilewidth);
+        this.tileset = new Tileset(map_json.tilesets.image,map_json.tilesets.firstgid,map_json.tilesets.columns,map_json.tilesets.spacing,map_json.tilesets.tileheight,map_json.tilesets.tilewidth);
         this.height = map_json.height;
         this.width = map_json.width;
-        this.tileCount = tempTileset.tilecount;
-        let tempData = JSON.parse(JSON.parse(map_json).layers[0]).data;
+        this.tileCount = map_json.layers[0].data.length;
+        let tempData = map_json.layers[0].data;
         for(var _i = 0;_i < this.tileCount;_i++){
             let tempTile = new Tile(tempData[_i*3],tempData[_i*3 + 1],tempData[_i*3 + 2],0);
             this.tileArray.push(tempTile);
