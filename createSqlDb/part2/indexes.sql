e5cfe86c0c8344cf0fa92022e4dd6962067993c3
mihaivasilache <mihai.vasilache12@yahoo.com>
2017-05-29 20:20:32 +0300

    BD part 3 - nici nu mai explic ca nu are rost

--- createSqlDb/part2/indexes.sql
+++ createSqlDb/part2/indexes.sql
@@ -1,3 +1,5 @@
 create index matchByPlayer1 on match(player1);
 create index matchByPlayer2 on match(player2);
 create index troopByLoadout on troop(loadoutId);
+drop index troopModifier;
+create index troopModifier on troopModifier(modifierId,troopId);
