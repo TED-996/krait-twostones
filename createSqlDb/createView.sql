74db3c8f430ff6102a1b098516e4c39123c7cd8f
mihaivasilache <mihai.vasilache12@yahoo.com>
2017-05-30 12:10:19 +0300

    BD part 3 - trebuie verificat indecsii

--- createSqlDb/createView.sql
+++ createSqlDb/createView.sql
@@ -1,39 +1,24 @@
+create or replace function getMax(a number,b number ) return number as
+begin
+    if a > b then 
+        return a;
+    else 
+        return b;
+    end if;
+end getMax;
+/
 drop materialized view TroopStatsCalculator;
 /
-create materialized view TroopStatsCalculator as
+create materialized view TroopStatsCalculator 
+refresh force on demand
+as
     select  troop.id,troop.classId,troop.loadoutId,troop.skinId,
-            (troopClass.maxHp     + ((troopClass.maxHp     * sum(modifier.maxHp    ))/100)) as "MAXHP",
-            (troopClass.dmg       + ((troopClass.dmg       * sum(modifier.dmg      ))/100)) as "DMG",
-            (troopClass.atkRange  + ((troopClass.atkRange  * sum(modifier.atkRange ))/100)) as "ATKRANGE",
-            (troopClass.moveRange + ((troopClass.moveRange * sum(modifier.moveRange))/100)) as "MOVERANGE" from
+            GREATEST ((troopClass.maxHp     + ((troopClass.maxHp     * sum(modifier.maxHp    ))/100)),0.001) as "MAXHP",
+            GREATEST ((troopClass.dmg       + ((troopClass.dmg       * sum(modifier.dmg      ))/100)),0.001) as "DMG",
+            GREATEST ((troopClass.atkRange  + ((troopClass.atkRange  * sum(modifier.atkRange ))/100)),0) as "ATKRANGE",
+            GREATEST ((troopClass.moveRange + ((troopClass.moveRange * sum(modifier.moveRange))/100)),0) as "MOVERANGE" from
             troop join troopModifier on troop.id = troopModifier.troopId 
                   join troopClass    on troop.classId = troopClass.id
                   join modifier      on troopModifier.modifierId = modifier.id
             group by troop.id,troop.classId,troop.loadoutId,troop.skinId,troopModifier.troopId,troopClass.maxHp,troopClass.dmg,troopClass.atkRange,troopClass.moveRange;
 /
-create or replace trigger onDeleteLoadout
-after delete on Loadout
-for each row
-declare
-    pragma autonomous_transaction; 
-begin
-  delete from troop where loadoutid = :old.id;
-  commit;
-end;
-/
-create or replace trigger TroopModifierRefresh
-after insert or update or delete on TroopModifier
-for each row
-declare 
-    l_job number;
-begin 
-    dbms_job.submit( l_job, 'dbms_mview.refresh( ''TROOPSTATSCALCULATOR'' );' ); 
-end;
-/
---tests
-delete from troopmodifier where troopid = 1 and modifierid = 2;
-commit;
-insert into troopmodifier values (1,2);
-commit;
-select * from Troopstatscalculator order by id;
-
--- createSqlDb/part2/indexes.sql
+++ createSqlDb/part2/indexes.sql
@@ -1,5 +1,15 @@
+drop index matchByPlayer1;
+drop index matchByPlayer2;
+drop index troopByLoadout;
+drop index troopModifierByModifier;
+drop index skinByClass;
+drop index troopByClassId;
+drop index loadoutByPlayerId;
+
 create index matchByPlayer1 on match(player1);
 create index matchByPlayer2 on match(player2);
 create index troopByLoadout on troop(loadoutId);
-drop index troopModifier;
-create index troopModifier on troopModifier(modifierId,troopId);
+create index troopModifierByModifier on troopModifier(modifierId,troopId);
+create index skinByClass on skin(classId);
+create index troopByClassId on troop(classId);
+create index loadoutByPlayerId on Loadout(playerId);
