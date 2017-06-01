fb04398155435abe1b308fde2afdcfd0a7047bbb
mihaivasilache <mihai.vasilache12@yahoo.com>
2017-06-02 01:04:26 +0300

    solved bugs added already in queue pop-up

--- site-root/js/canvas.js
+++ site-root/js/canvas.js
@@ -1,30 +1,29 @@
 //Get the button that serches for a match
 var findMatch = document.getElementById("findMatchBtn");
-console.log(findMatch);
+;
 //Get the cancel button to interrupt the search
 var cancelBtn = document.getElementById("cancelBtn");
-console.log(cancelBtn);
+
 //Socket for webSockets
 var socket;
 
 
 findMatch.onclick = function() {
     socket = new WebSocket(getWebSocketUrl("/queue_wait"),"queueProtocol");
-    //searchMatch.style.display = "block"
     socket.onmessage = function (msg) {
         if (msg.data === "already_in_queue"){
+            $('#myModal').modal('hide');
+            $('#inQueue').modal('show')
             console.log("already in match");
             socket.close(); //trebuie facut aici o chestie care sa zica ca esti deja in queue
-            //searchMatch.style.display = "none";
-            console.log("already in match")
+
+
         }
     }
 };
 
 cancelBtn.onclick = function() {
-    //searchMatch.style.display = "none";
     socket.send("exit_queue");
-    time.sleep(1);
     socket.close()
 };
 
