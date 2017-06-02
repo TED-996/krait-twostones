d8276852e0aa8692f86d04bf884754cadea067c4
ted_996 <ebudaca@ias.bitdefender.biz>
2017-06-02 18:29:18 +0300

    Automated matchmaker client

--- site-root/.config/init.py
+++ site-root/.config/init.py
@@ -5,5 +5,11 @@ from db_access import db_ops
 import json
 import urllib
 from misc import debug_config
+import subprocess
+import os
+
 
 db_ops.read_store_password()
+
+subprocess.Popen(
+    ["python", os.path.normpath(os.path.join(krait.site_root, "..", "matchmaker", "matchmaker_client.py"))])
