afb487fb68389a2fec96ac99123eb438819abe3a
mihaivasilache <mihai.vasilache12@yahoo.com>
2017-06-03 20:17:42 +0300

    generated loadout when registered

--- createSqlDb/populateTables.sql
+++ createSqlDb/populateTables.sql
@@ -3,3 +3,5 @@ start "createTables.sql";
 start "./part2/sitePackage.sql";
 start "./part2/indexes.sql";
 start "./part2/triggers.sql";
+start "populateTroopclass.sql";
+start "populateModifiers.sql";
