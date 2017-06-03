d3541ccde515fbc48e36854ba3fcdd14a2dcff81
MihaiMaganu <mihai.maganu@info.uaic.ro>
2017-06-03 14:19:12 +0300

    header remastered

--- site-root/.py/ctrl/dashboard.py
+++ site-root/.py/ctrl/dashboard.py
@@ -4,6 +4,8 @@ from auth_utils import auth_tests
 class DashboardController(object):
     def __init__(self):
         self.username = auth_tests.get_auth()
+        self.level = 0
+        self.mmr = 120
     
     def get_view(self):
         return ".view/dashboard.html"
