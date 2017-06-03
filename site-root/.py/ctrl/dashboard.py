0f1f6369e53e6260b9fe99cb4633ecd4a1f9a938
ted_996 <ebudaca@ias.bitdefender.biz>
2017-06-03 22:16:29 +0300

    Dashboard changes, idk

--- site-root/.py/ctrl/dashboard.py
+++ site-root/.py/ctrl/dashboard.py
@@ -13,23 +13,11 @@ class DashboardController(object):
         self.level = player.player_level
         self.mmr = player.mmr
         
-        #self.active_loadout = db_loadout.get_by_id(player.loadout_id)
-        #self.user_loadouts = db_loadout.get_all_by_id(player.id)      
-        #self.number_of_loadouts = len(self.user_loadouts)
-        
-
-        self.active_loadout = loadout.Loadout(1, player.id, "loadout smecher")
-        
-        self.user_loadouts = []
-        self.user_loadouts.append(loadout.Loadout(1, player.id, "loadout smecher"))
-        self.user_loadouts.append(loadout.Loadout(2, player.id, "dani mocanu"))
-        self.user_loadouts.append(loadout.Loadout(3, player.id, "loadout mare mafiot"))
-        self.user_loadouts.append(loadout.Loadout(4, player.id, "suflet de golan"))
-        self.number_of_loadouts = 4
+        self.active_loadout = db_loadout.get_by_id(player.loadout_id)
+        self.user_loadouts = db_loadout.get_all_by_id(player.id)
 
     def get_view(self):
         return ".view/dashboard.html"
 
     def make_active_loadout(self, loadout_obj):
-            self.active_loadout = loadout_obj
-            return "makeActiveLoadout(this.id);"
+        return "makeActiveLoadout(this.id);"
