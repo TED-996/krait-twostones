ce49bfce0f484bc857c6fec7c73e6b04985bbea4
MihaiMaganu <mihai.maganu@info.uaic.ro>
2017-06-03 14:25:21 +0300

    gameplay header

--- site-root/.py/ctrl/gameplay.py
+++ site-root/.py/ctrl/gameplay.py
@@ -1,8 +1,11 @@
 import mvc
+from auth_utils import auth_tests
 
 class GameplayController(object):
     def __init__(self):
-        pass
+        self.username = auth_tests.get_auth()
+        self.level = 0
+        self.mmr = 120
     
     def get_view(self):
         return ".view/gameplay.html"
