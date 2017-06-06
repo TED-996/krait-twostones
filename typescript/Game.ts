29b1cc5fde2ee8617b16c7f2de2eb4ab5d3aeb48
mihaivasilache <mihai.vasilache12@yahoo.com>
2017-06-06 10:56:22 +0300

    added text on gameplay and on_delete_match

--- typescript/Game.ts
+++ typescript/Game.ts
@@ -31,6 +31,7 @@ class WegasGame
     activeTroop : GameTroop;
 
     endTurn : Phaser.Button;
+    scoreLabel : Phaser.Text;
 
     constructor()
     {
@@ -77,9 +78,12 @@ class WegasGame
             4, 3, 5, 3
         );
         this.fgGroup.add(this.endTurn);
-        this.fgGroup.fixedToCamera = true;
+        this.scoreLabel = this.game.add.text(50, 50, '0 : 0', { font: "65px Arial", fill: "##ff0044 ", align: "center" });
+
+        this.fgGroup.add(this.scoreLabel);
         this.updateEndTurn(this.gameController.inTurn);
 
+        this.fgGroup.fixedToCamera = true;
 
         AllOptions.loadAjax();
 
